76532fa39f05373d0df0890c6f903a96c4523b2f
hjudgex <hjudgex@localhost>
2026-05-12 23:16:25 -0700

    chore: add .gitignore for Android build caches, remove tracked Gradle/CXX artifacts

--- swarmchat/android/app/.cxx/Debug/65c6j67u/prefab/x86/prefab/lib/i686-linux-android/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-reanimated::reanimated)
-add_library(react-native-reanimated::reanimated SHARED IMPORTED)
-set_target_properties(react-native-reanimated::reanimated PROPERTIES
-    IMPORTED_LOCATION "/home/hjudgex/projects/swarmchat/swarmchat/node_modules/react-native-reanimated/android/build/intermediates/cxx/Debug/3un2ly4d/obj/x86/libreanimated.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/home/hjudgex/projects/swarmchat/swarmchat/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
